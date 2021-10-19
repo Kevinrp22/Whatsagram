3433dc54e8fbbfa9d3268a5912d36ec15ae1eab0
Kevin <kevinpuchaicela@gmail.com>
2021-10-19 11:41:06 +0200

    laptop to pc

---
+++ readme.md
@@ -0,0 +1,25 @@
+### TODO LIST WHATSAGRAM
+
+- [ ] INTER
+- [] MESSAG
+- [] TEXTAR
+- [] KEYB
+- [ ] EMOJI
+- [ ] CEC
+- [ ] DELECT
+- [ ] MAJUS
+- [ ] ESPAI
+- [ ] SALT
+- [ ] GRID
+- [ ] FLEX
+- [ ] RESP
+- [ ] CENTR
+- [ ] KEYS
+- [ ] FONTS
+- [ ] VARS
+- [ ] STYLE
+- [ ] CORRE
+- [ ] SEPAR
+# - EXTRA -
+- [ ] EMOJI
+- [ ] PENJA
--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ const keys = [
   { key: "k" },
   { key: "l" },
   { key: "ñ" },
-  { key: "⇧" , specialClass:"mayus-key"},
+  { key: "⇧", specialClass: "mayus-key" },
   { key: "z" },
   { key: "x" },
   { key: "c" },
@@ -27,7 +27,7 @@ const keys = [
   { key: "b" },
   { key: "n" },
   { key: "m" },
-  { key: "←", specialClass:"delete-key" },
+  { key: "←", specialClass: "delete-key" },
   { key: "," },
   { key: "‎ ‏", specialClass: "space-key" },
   { key: "." },
@@ -36,10 +36,54 @@ const keys = [
   { key: "→" },
   { key: "↲" },
 ];
-const buttons = document.querySelector(".keyboard__buttons");
+const messages = [];
+const inputMessage = document.querySelector("#keyboard__header__input");
+const contentMessages = document.querySelector("#messages");
 
-keys.forEach((element) => {
-  buttons.innerHTML += `<button class="keyboard__buttons__key ${
-    element.specialClass || ''
-  }">${element.key}</button>`;
-});
+function renderButtons() {
+  const buttons = document.querySelector("#keyboard__buttons");
+
+  keys.forEach((element) => {
+    buttons.innerHTML += `<button class="keyboard__buttons__key ${
+      element.specialClass || ""
+    }"
+    onclick="pressKey('${element.key}')"
+    >${element.key}</button>`;
+  });
+}
+
+function pressKey(key) {
+  inputMessage.value += key;
+}
+
+function sendMessage() {
+  const date = new Date();
+  const newMessage = {
+    id: 1,
+    date: {
+      day: date.toLocaleDateString("es", { weekday: "long" }), // Lunes, martes, miercoles, jueves, viernes, etc..
+      month: date.getMonth(),
+      year: date.getFullYear(),
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+    },
+    message: inputMessage.value,
+  };
+  messages.push(newMessage);
+  console.log(messages);
+  inputMessage.value = ""
+  renderMessages();
+}
+
+function renderMessages() {
+  messages.forEach((item) => {
+    contentMessages.innerHTML += `
+    <div class="message">
+          <span class="message__text">${item.message}</span>
+          <span class="message__date">${item.date.hour}:${item.date.minute > 10 ? item.date.minute : "0" + item.date.minute}</span>
+    </div>
+    `;
+  });
+}
+
+window.addEventListener("load", renderButtons);
